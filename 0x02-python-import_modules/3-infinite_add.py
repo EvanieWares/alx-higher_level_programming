a5ed960f12525c0a7f45a0d8aa37b9b9844494a7
evaniewares <chisopsyelera@gmail.com>
2023-09-07 20:21:17 +0000

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 import sys
-import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("numbers", nargs="+", type=int)
-    args = parser.parse_args()
-    numbers = args.numbers
-
+    argv = sys.argv[1:]
+    argc = len(argv)
     total = 0
-    for number in numbers:
-        total += number
+
+    if argc > 0:
+        for i in range(argc):
+            total += int(argv[i])
 
     print(total)
 
