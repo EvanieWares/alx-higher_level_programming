fa30f591a78f61607394206564a461477456c6f6
evaniewares <chisopsyelera@gmail.com>
2023-09-07 19:37:10 +0000

    Task 2, 3 and 5

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 from sys import argv
 
-result = 0
 if __name__ == "__main__":
+    result = 0
     if len(argv) != 1:
         for i in range(1, len(argv)):
             result = result + int(argv[i])
-        print(result)
+        print("{}".format(result))
