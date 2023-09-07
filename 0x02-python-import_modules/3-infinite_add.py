cb6edf8dc3845d37992a3e140ca269128a7c0bb2
evaniewares <chisopsyelera@gmail.com>
2023-09-07 19:28:30 +0000

    Task 3 to task 5

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+
+result = 0
+if __name__ == "__main__":
+    if len(argv) != 1:
+        for i in range(1, len(argv)):
+            result = result + int(argv[i])
+        print(result)
