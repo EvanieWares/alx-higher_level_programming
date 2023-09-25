5477ffd6be92695612e60a4d35e05debb2aa3fcc
evaniewares <chisopsyelera@gmail.com>
2023-09-25 20:53:03 +0000

    task 7 and 8

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as val:
+        print("Exception: {}".format(val), file=sys.stderr)
+        return False
