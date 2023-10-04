39118c1c5626a7844f45e8375685c0fd624b2258
evaniewares <chisopsyelera@gmail.com>
2023-10-04 19:45:58 +0000

    task 30

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,7 +6,8 @@ A class with no class or object attribute
 
 class LockedClass:
     """
-    A class that prevents the dynamic creation of new instance attributes, except for 'first_name'.
+    A class that prevents the dynamic creation of new instance attributes,
+    except for 'first_name'.
     """
 
     def __setattr__(self, name, value):
@@ -18,7 +19,8 @@ class LockedClass:
             value: The value to be assigned to the attribute.
 
         Raises:
-            AttributeError: If the attribute is not 'first_name' and it does not already exist.
+            AttributeError: If the attribute is not 'first_name'
+            and it does not already exist.
         """
         if not hasattr(self, name) and name != 'first_name':
             raise AttributeError(
