2187e340a804e0b60a4480febddc5844c27b7d80
evaniewares <chisopsyelera@gmail.com>
2023-10-12 19:36:48 +0000

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,18 +2,15 @@
 # 0-read_file
 """Implements a function that reads a text file"""
 
-
 def read_file(filename=""):
-    """
-    Reads a text file (UTF8) and prints it to stdout
+	"""
+	Reads a text file (UTF8) and prints it to stdout
 
-    Args:
-    filename (string): name of the file to read
-    """
-    try:
-        with open(filename, mode='r', encoding='utf-8') as file:
-            print(file.read())
-    except:
-        pass
+	Args:
+	filename (string): name of the file to read
+	"""
+	with open(filename, encoding="utf-8") as file:
+		print(file.read(), end="")
+
 
 read_file("text.txt")
