6debdaccf14cf6fbae6e21b74c2cc81b2e42a71b
evaniewares <chisopsyelera@gmail.com>
2023-10-22 10:54:32 +0000

    task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Implements a class Student"""
+
+
+class Student:
+    """
+    A class Student that defines a student by:
+            - first_name
+            - last_name
+            - age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes the Student class
+
+        Args:
+        first_name (string): The first name of the student
+        last_name (string): The last name of the student
+        age (int): The age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
