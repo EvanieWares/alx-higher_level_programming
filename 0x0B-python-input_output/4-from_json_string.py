54a831e1d4bf79f0ce80589c9aadfce37f4611c5
evaniewares <chisopsyelera@gmail.com>
2023-10-22 08:46:39 +0000

    task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# 4-from_json_string
+"""
+Implements a function that returns an object (Python data structure)
+represented by a JSON string
+"""
+
+
+def from_json_string(my_str):
+    """
+    Returns an object represented by a JSON string
+
+    Args:
+    my_str (string): string
+
+    Return:
+    Object represented by a JSON string
+    """
+    import json
+    return json.loads(my_str)
