62125a808e5e8a493ed8742e96fef9637a59a954
evaniewares <chisopsyelera@gmail.com>
2023-11-09 12:39:17 +0000

    task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -145,19 +145,31 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """
+        Return the bigger rectangle between the two
+        
+        Args:
+        rect_1 (object): rectangle 1
+        rect_2 (object): rectangle 2
+        
+        Raises:
+        TypeError: if rect_1 and rect_2 are not instances of Rectangle
+        
+        Returns:
+        The bigger rectangle
+        """
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        area_1 = rect_1.area
-        area_2 = rect_2.area
-        if rect_1.area() >= rect_2.area():
+        area_1 = rect_1.area()
+        area_2 = rect_2.area()
+        if area_1 >= area_2:
             return rect_1
-        else:
-            return rect_2
+        return rect_2
 
     @classmethod
-    def square(cls, size):
+    def square(cls, size=0):
         """
         Creates a new Rectangle instance as a square
 
