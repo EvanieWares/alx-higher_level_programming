263317be5e6743ed356b100853406a5ff3005406
evaniewares <chisopsyelera@gmail.com>
2023-12-16 22:31:34 +0200

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Implements a script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username, password, database, name = sys.argv[1:5]
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        password=password,
+        db=database,
+        charset="utf8",
+    )
+    cursor = connection.cursor()
+    cursor.execute(
+        f"\
+                   SELECT * FROM states \
+                   WHERE name = '{name}' \
+                   ORDER BY id ASC"
+    )
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+    cursor.close()
+    connection.close()
