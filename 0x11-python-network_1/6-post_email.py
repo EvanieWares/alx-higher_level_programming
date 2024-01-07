ea46e51af80f6c088db0128556c3a0654fce2b3e
evaniewares <chisopsyelera@gmail.com>
2024-01-07 23:19:51 +0200

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,21 +7,8 @@ displays the body of the response
 import requests
 import sys
 
-
-def post_email(url, email):
-    """
-    Sends a POST request to a URL and displays the body of the response
-
-    Args:
-        url (string): The url to fetch
-        email (string): The email to post
-    """
-    # print(help(requests))
+if __name__ == '__main__':
+    url, email = sys.argv[1:3]
     data = dict(email=email)
     r = requests.post(url, data)
     print(r.text)
-
-
-if __name__ == '__main__':
-    url_string, email_string = sys.argv[1:3]
-    post_email(url_string, email_string)
