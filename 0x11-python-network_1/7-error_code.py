28775a3d30236ff624103bf1920278743e7cfdfc
evaniewares <chisopsyelera@gmail.com>
2024-01-07 23:20:06 +0200

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,20 +6,9 @@ displays the body of the response.
 import requests
 import sys
 
-
-def error_code(url):
-    """
-    Displays the body of the response
-
-    Args:
-        url (string): The url to fetch
-    """
-    r = requests.get(url)
+if __name__ == '__main__':
+    r = requests.get(sys.argv[1])
     if r.status_code >= 400:
         print(f'Error code: {r.status_code}')
     else:
         print(r.text)
-
-
-if __name__ == '__main__':
-    error_code(sys.argv[1])
