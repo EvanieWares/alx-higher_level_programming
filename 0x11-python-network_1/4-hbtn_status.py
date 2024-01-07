45cc272fcf868a1f7525d6f9ceb34bcecab3cf83
evaniewares <chisopsyelera@gmail.com>
2024-01-07 23:18:50 +0200

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,21 +4,10 @@ Implements a script that fetches https://alx-intranet.hbtn.io/status.
 """
 import requests
 
-
-def fetch_url(url):
-    """
-    Fetches a URL
-
-    Args:
-        url (string): The url to fetch
-    """
+if __name__ == '__main__':
+    url = "https://alx-intranet.hbtn.io/status"
     r = requests.get(url)
     body = r.text
     print('Body response:')
     print(f'\t- type: {type(body)}')
     print(f'\t- content: {body}')
-
-
-if __name__ == '__main__':
-    url_string = "https://alx-intranet.hbtn.io/status"
-    fetch_url(url_string)
