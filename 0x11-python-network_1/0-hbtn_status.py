144c8360b027d308782c9784ab02e2e26550eba6
evaniewares <chisopsyelera@gmail.com>
2024-01-07 08:37:37 +0200

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
 """
-Implements a script that fetches https://alx-intranet.hbtn.io/status.
+Module that fetches https://alx-intranet.hbtn.io/status.
 """
 import urllib.request
 
-
-def fetch_url(url):
-    """
-    Fetches a URL
-
-    Args:
-        url (string): The url to fetch
-    """
-    with urllib.request.urlopen(url) as response:
-        content = response.read()
-        print('Body response:')
-        print(f'\t- type: {type(content)}')
-        print(f'\t- content: {content}')
-        print(f'\t- utf8 content: {response.msg}')
-
-
 if __name__ == '__main__':
-    url_string = "https://alx-intranet.hbtn.io/status"
-    fetch_url(url_string)
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print('Body response:')
+        print(f'    - type: {type(body)}')
+        print(f'    - content: {body}')
+        print(f'    - utf8 content: {body.decode("utf-8")}')
