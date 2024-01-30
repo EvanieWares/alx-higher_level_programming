ab3b240296a46358f2bc4166acfe05a8f5b66bb5
evaniewares <chisopsyelera@gmail.com>
2024-01-30 16:54:38 +0200

    JavaScript Web - updated 4-1

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,19 +3,18 @@
 const request = require('request');
 
 const apiUrl = process.argv[2];
-const characterId = 18;
+let count = 0;
 
-request.get(apiUrl, (error, response, body) => {
+request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
     return;
   }
 
   const films = JSON.parse(body).results;
-  let count = 0;
 
   films.forEach(film => {
-    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+    if (film.characters && film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
       count++;
     }
   });
